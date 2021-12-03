dcf1386ac56865cea5ffa59be30048a0adf44ebd
wuchao5 <wuchao5@staff.weibo.com>
2021-12-03 20:03:12 +0800

    底部播放控件

--- app/src/main/java/com/wuc/music/ui/page/PlayerFragment.kt
+++ app/src/main/java/com/wuc/music/ui/page/PlayerFragment.kt
@@ -5,56 +5,77 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import com.wuc.music.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.wuc.music.bridge.state.PlayerViewModel
+import com.wuc.music.databinding.FragmentPlayerBinding
+import com.wuc.music.ui.base.BaseFragment
 
 /**
- * A simple [Fragment] subclass.
- * Use the [PlayerFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * @author : wuchao5
+ * @date : 2021/12/3 19:59
+ * @desciption : 底部播放条的 Fragment
  */
-class PlayerFragment : Fragment() {
-  // TODO: Rename and change types of parameters
-  private var param1: String? = null
-  private var param2: String? = null
+class PlayerFragment : BaseFragment() {
+
+  private var mPlayerViewModel: PlayerViewModel? = null
+  private var mBinding: FragmentPlayerBinding? = null
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    arguments?.let {
-      param1 = it.getString(ARG_PARAM1)
-      param2 = it.getString(ARG_PARAM2)
-    }
+    // 初始化 VM
+    mPlayerViewModel = getFragmentViewModelProvider(this).get<PlayerViewModel>(PlayerViewModel::class.java)
   }
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_player, container, false)
+    // 加载界面
+    val view: View = inflater.inflate(R.layout.fragment_player, container, false)
+
+    // 绑定 Binding
+    mBinding = FragmentPlayerBinding.bind(view)
+    mBinding?.click = ClickProxy()
+    mBinding?.event = EventHandler()
+    mBinding?.vm = mPlayerViewModel
+    return view
   }
 
-  companion object {
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PlayerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    @JvmStatic
-    fun newInstance(param1: String, param2: String) =
-      PlayerFragment().apply {
-        arguments = Bundle().apply {
-          putString(ARG_PARAM1, param1)
-          putString(ARG_PARAM2, param2)
-        }
-      }
+  /**
+   * 当我们点击的时候，我们要触发
+   */
+  inner class ClickProxy {
+
+    /*public void playerMode() {
+        PlayerManager.getInstance().changeMode();
+    }*/
+
+    fun previous() = PlayerManager.instance.playPrevious()
+
+    operator fun next() = PlayerManager.instance.playNext()
+
+    // 点击缩小的
+    fun slideDown() = sharedViewModel.closeSlidePanelIfExpanded.setValue(true)
+
+    //　更多的
+    fun more() {}
+
+    fun togglePlay() = PlayerManager.instance.togglePlay()
+
+    fun playMode() = PlayerManager.instance.changeMode()
+
+    fun showPlayList() = showShortToast("最近播放的细节，我没有搞...")
+  }
+
+  /**
+   * 专门更新 拖动条进度相关的
+   */
+  inner class EventHandler : SeekBar.OnSeekBarChangeListener {
+    override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {}
+    override fun onStartTrackingTouch(seekBar: SeekBar) {}
+
+    // 一拖动 松开手  把当前进度值 告诉PlayerManager
+    override fun onStopTrackingTouch(seekBar: SeekBar) = PlayerManager.instance.setSeek(seekBar.progress)
   }
 }
